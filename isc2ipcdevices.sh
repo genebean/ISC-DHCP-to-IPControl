8bce3d0ea1c86f065835f8a9d7ec8bc5253d046d
GeneBean <me@technicalissues.us>
2014-02-21 21:14:37 -0500

    Updated comments and corrected the IPControl-specific items to only be used if there is a hostname

--- isc2ipcdevices.sh
+++ isc2ipcdevices.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/awk -f
 # 
-# Author: Matt Pascoe - matt@opennetadmin.com 
+# Author: GeneBean
+#
+# The original version of this script was written by 
+# Matt Pascoe - matt@opennetadmin.com  and was for a different use.
 #
 # This awk script is used to extract relavant information from a dhcpd.conf
-# config file and build a csv with appropriate fields for passing into
-# a dcm.pl module.  This can be used to bootstrap a new database from existing
-# site data.  As usual, inspect the output for accuracy.
- 
-# Note that for hosts, it will try a reverse lookup on the IP address
-# to determine an appropriate dns name
+# config file and build a csv with appropriate fields for importing into
+# IPControl.  As usual, inspect the output for accuracy.
+#
 #
 # USAGE:
-# make this script executable with `chmod +x dhcpparse.awk` and then
+# make this script executable with `chmod +x isc2ipcdevices.sh` and then
 #
-#   cat dhcpd.conf|./dhcpparse.awk
+#   cat dhcpd.conf|./isc2ipcdevices.sh
  
 # set the RECORD SEPARATOR, RS, to "}" ... records span multiple lines
 BEGIN {RS="}"}
- 
-# TODO: figure out how to skip comment lines without having to use sed first
- 
+
 length($0) > 18 { total++
  
   for(i=1;i<=NF;i++) {
@@ -55,22 +53,23 @@ length($0) > 18 { total++
     }
   }
 
-  addressType[total] = "Manual DHCP"
-  deviceType[total] = "Unspecified"
-  hwType[total] = "ethernet"
-  resourceRecordFlag[total] = "FALSE"
-  domainName[total] = "westga.edu"
-  container[total] = ""
-  domainTyp[total] = "Default"
-  description[total] = ""
-  userDefinedFields[total] = ""
-  aliases[total] = ""
-  dupWarning[total] = ""
-  interfaces[total] = "Default"
-  excludeFromDiscovery[total] = "FALSE"
-  virtual[total] = "FALSE"
-  DUID[total] = ""
-  
+  if( length(hostname[total]) > 0 ) {  
+    addressType[total] = "Manual DHCP"
+    deviceType[total] = "Unspecified"
+    hwType[total] = "ethernet"
+    resourceRecordFlag[total] = "FALSE"
+    domainName[total] = "westga.edu"
+    container[total] = ""
+    domainTyp[total] = "Default"
+    description[total] = ""
+    userDefinedFields[total] = ""
+    aliases[total] = ""
+    dupWarning[total] = ""
+    interfaces[total] = "Default"
+    excludeFromDiscovery[total] = "FALSE"
+    virtual[total] = "FALSE"
+    DUID[total] = ""
+  }
  
 }
  
