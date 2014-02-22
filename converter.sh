d8741f04dd3607609c339f1301a8a5c693a4d4ef
GeneBean <me@technicalissues.us>
2014-02-21 21:48:00 -0500

    Added usage guide

--- converter.sh
+++ converter.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-input=$1
-
-cat $input |./isc2ipcdevices.sh |sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n 
+if [ "$#" -ne 1 ]; then
+    echo "To use this script just pass the path to your dhcpd.conf file"
+    echo "or files as an argument wrapped in quotes"
+    echo "Ex: ./converter.sh \"/etc/dhcp/1*\""
+else
+    input=$1
+    cat $input |./isc2ipcdevices.sh |sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n 
+fi
